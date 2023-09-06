601265459a08b518691f7b46e7abf17159ac54f0
Louis Gaumond <2033838@etu.cchic.ca>
2023-09-06 11:31:42 -0400

    echafaudé Classes, Race et Enchantement

--- Api_DnD_project/Api_DnD/Model/Enchantement.cs
+++ Api_DnD_project/Api_DnD/Model/Enchantement.cs
@@ -2,5 +2,40 @@
 {
     public class Enchantement
     {
+        public string Nom { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public int Modif { get; set; }
+        public int Id { get; set; }
+
+        public Enchantement GetEnchantementById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Enchantement> GetAllEnchantement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Enchantement GetRandomEnchantement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PostEnchantement(string nom, string description, string type, int modif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PutEnchantement(string nom, string description, string type, int modif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEnchantement(string nom)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Api_DnD_project/Api_DnD/Model/Race.cs
+++ Api_DnD_project/Api_DnD/Model/Race.cs
@@ -2,5 +2,43 @@
 {
     public class Race
     {
+        public string Nom { get; set; }
+        public int BonusPV { get; set; }
+        public int BonusDex { get; set; }
+        public int BonusForce { get; set; }
+        public int BonusIntel { get; set; }
+        public int BonusWisdom { get; set; }
+        public int BonusConsti { get; set; }
+        public int Id { get; set; }
+
+        public Race GetRaceByName(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Race> GetAllRace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Race GetRandomRace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PostRace(string nom, int pv, int dex, int force, int intel, int wisdom, int consti)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PutRace(string nom, int pv, int dex, int force, int intel, int wisdom, int consti)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRace(string nom)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
