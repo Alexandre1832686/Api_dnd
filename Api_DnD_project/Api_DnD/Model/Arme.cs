a5db090955935d16bca1c6dd8c6701e88d99c0d5
MathieuDionne2032450 <2032450@etu.cchic.ca>
2023-09-11 16:47:04 -0400

    Arme et Armure

--- Api_DnD_project/Api_DnD/Model/Arme.cs
+++ Api_DnD_project/Api_DnD/Model/Arme.cs
@@ -2,5 +2,16 @@
 {
     public class Arme
     {
+        public int BonusJet { get; set; }
+
+        public int BonusForce { get; set; }
+
+        public string Nom { get; set; }
+
+        public Enchantement Enchantement { get; set; }
+
+        public int id { get; set; }
+
+       
     }
 }
--- Api_DnD_project/Api_DnD/Model/Armure.cs
+++ Api_DnD_project/Api_DnD/Model/Armure.cs
@@ -2,5 +2,20 @@
 {
     public class Armure
     {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Ac { get; set; }
+
+        public bool DexBonus { get; set; }
+
+        public int MaxDexMod { get; set; }
+
+        public bool StealthDisadvantage { get; set; }
+
+        public Enchantement Enchant { get; set; }
+
+        public int Id { get; set; }
     }
 }
