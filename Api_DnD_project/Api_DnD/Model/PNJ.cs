c19ce40a9639b3fc4421aed76483928dd9c4486c
MathieuDionne2032450 <2032450@etu.cchic.ca>
2023-09-18 16:46:12 -0400

    DTO et PNJ

---
+++ Api_DnD_project/Api_DnD/Model/PNJ.cs
@@ -0,0 +1,21 @@
+﻿namespace Api_DnD.Model
+{
+    public class PNJ
+    {
+        public int[] Physique { get; set; }
+
+        public string Description;
+
+        public string Name;
+        
+        public string[] Quête { get; set; }
+
+        public PNJ(int[] physique, string description, string name, string[] quête)
+        {
+            Physique = physique;
+            Description = description;
+            Name = name;
+            Quête = quête;
+        }
+    }
+}
