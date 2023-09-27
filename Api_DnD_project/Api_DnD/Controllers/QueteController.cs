77ae7e23b4d6c25fb7c287859798e3f5b52b798f
Louis Gaumond <2033838@etu.cchic.ca>
2023-09-27 09:49:52 -0400

    im about to bus

---
+++ Api_DnD_project/Api_DnD/Controllers/QueteController.cs
@@ -0,0 +1,25 @@
+﻿using Api_DnD.Data;
+using Api_DnD.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_DnD.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QueteController : Controller
+    {
+        private readonly DNDContext _context;
+
+        public QueteController(DNDContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Quete>>> GetQuete()
+        {
+            return await _context.Quetes.Include(x => )
+        }
+    }
+}
